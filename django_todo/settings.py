3ee95c7067d558d439ecf56e46405459554dff29
Aramantos <john.doyle.mail@icloud.com>
2021-06-20 18:37:32 +0000

    really aint got time

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://qahhckovznbywo:dbf387e44367ea1c22819ecac27d24a9a89377dc2c008e2a576d1e34dbd22705@ec2-54-73-58-75.eu-west-1.compute.amazonaws.com:5432/dcrpqbhcopt863')
 }
 
 
